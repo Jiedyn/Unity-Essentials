619a7f1de427482d809729c1e4c5537c8af192aa
Jie <Masterjiedyn@gmail.com>
2025-03-03 17:07:40 +1100

    Progress on Bonus Challenge Mission 6 - Collectable sound effects, background music, 3D UI Script and interface

--- Assets/_Unity Essentials/Scripts/Collectable.cs
+++ Assets/_Unity Essentials/Scripts/Collectable.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-
     public float rotationSpeed;
+    public AudioSource soundEffect;
+
 
     public GameObject onCollectEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,11 +27,14 @@ public class Collectable : MonoBehaviour
        
         if(other.CompareTag("Player")) 
         {
-            // destroy the collectable
-            Destroy(gameObject);
+            // instantiate the pickup 
+            soundEffect.Play();
 
             // instantiate the particle effect
             Instantiate(onCollectEffect, transform.position, transform.rotation);
+
+            // destroy the collectable
+            Destroy(gameObject);
         }
     }
 }
